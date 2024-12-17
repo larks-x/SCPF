db4ac34e6ae29c72d8feb24cd15d6910ed250e23
larks-x <dearxx@larksx.com>
2024-12-17 17:48:25 +0800

    init

---
+++ src/IWindowView.cs
@@ -0,0 +1,15 @@
+﻿namespace SCPF
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IWindowView
+    {
+        int Width { get; }
+        int Height { get; }
+        int Left { get; }
+        int Top { get; }
+        void Create();
+
+    }
+}
